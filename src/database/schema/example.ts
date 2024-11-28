20d8bbb142a6d02098cbf414dce4e2f8ff96f6d8
iam-green <yundohyun050121@gmail.com>
2024-11-29 04:31:05 +0900

    remove: `database`

--- src/database/schema/example.ts
+++
@@ -1,6 +0,0 @@
-import { pgTable, uuid, timestamp } from 'drizzle-orm/pg-core';
-
-export const example = pgTable('example', {
-  id: uuid('id').primaryKey().defaultRandom(),
-  created: timestamp('created', { withTimezone: true }).notNull().defaultNow(),
-});
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import 'dotenv/config';
-import { databaseInit } from './database';
 
 async function bootstrap() {
-  await databaseInit();
   // Input Code Here
 }
 bootstrap();
